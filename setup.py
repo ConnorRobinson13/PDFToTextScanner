badeea3e5ece0ce7c031e557f2e215c92b1e3b85
Connor Robinson <cjrobin7@ncsu.edu>
2022-12-19 23:55:22 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PDFToTextScanner',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/ConnorRobinson13/PDFToTextScanner/tree/master',
+    license='',
+    author='Connor Robinson',
+    author_email='connorjr13@gmail.com',
+    description='A simple GUI that allows a user to convert a batch of PDF files to text files'
+)
