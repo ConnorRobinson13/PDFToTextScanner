18e4ec4792f1a42c03ac86930695d65788b45004
Connor Robinson <cjrobin7@ncsu.edu>
2022-12-19 23:52:01 -0500

    Added GUI and Instructions

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
-import os
-import glob
+import tkinter as tk
+from tkinter import filedialog
+
 import PyPDF2
 
-# Set the directory containing the PDF files
-pdf_dir = "input"
 
-# Check if the directory exists
-if not os.path.exists(pdf_dir):
-    raise ValueError("The specified directory does not exist")
+def select_files():
+    # Open a file selection dialog
+    files = filedialog.askopenfilenames(filetypes=[("PDF files", "*.pdf")])
+    # Get a list of the selected file paths
+    file_paths = list(files)
+    # Check if any files were selected
+    if not file_paths:
+        return
+    # Initialize an empty list to store the text from the PDF files
+    pdf_text = []
+    # Iterate through each PDF file
+    for file in file_paths:
+        # Open the PDF file in read-only mode
+        with open(file, 'rb') as pdf:
+            # Create a PDF object
+            pdf_reader = PyPDF2.PdfFileReader(pdf)
+            # Iterate through each page of the PDF
+            for page in range(pdf_reader.getNumPages()):
+                # Get the text from the current page
+                page_text = pdf_reader.getPage(page).extractText()
+                # Add the text to the list
+                pdf_text.append(page_text)
+    # Join the text from all the PDFs into a single string
+    all_text = '\n'.join(pdf_text)
+    # Open a save file dialog
+    output_file = filedialog.asksaveasfilename(filetypes=[("Text files", "*.txt")], defaultextension=".txt")
+    # Check if the user cancelled the save dialog
+    if not output_file:
+        return
+    # Write the text to the selected file
+    with open(output_file, "w", encoding="utf-8") as outfile:
+        outfile.write(all_text)
 
-# Initialize an empty list to store the text from the PDF files
-pdf_text = []
 
-# Use glob to get a list of all PDF files in the directory
-pdf_files = glob.glob(pdf_dir + "/*.pdf")
+# Create the GUI
+root = tk.Tk()
+root.title("PDF Converter")
 
-# Check if there are any PDF files in the directory
-if not pdf_files:
-    raise ValueError("No PDF files found in the specified directory")
+# Create a label to explain how to use the program
+instructions = tk.Label(root,
+                        text="To use this program, click the 'Select PDF files' button and select the PDF files you "
+                             "want to convert. Then choose a location to save the output file.")
+instructions.pack()
 
-# Iterate through each PDF file
-for file in pdf_files:
-    # Open the PDF file in read-only mode
-    with open(file, 'rb') as pdf:
-        # Create a PDF object
-        pdf_reader = PyPDF2.PdfFileReader(pdf)
-        # Iterate through each page of the PDF
-        for page in range(pdf_reader.getNumPages()):
-            # Get the text from the current page
-            page_text = pdf_reader.getPage(page).extractText()
-            # Add the text to the list
-            pdf_text.append(page_text)
+# Create a button to select the files
+select_button = tk.Button(root, text="Select PDF files", command=select_files)
+select_button.pack()
 
-# Join the text from all the PDFs into a single string
-all_text = '\n'.join(pdf_text)
-
-# Set the output file path
-output_file = "output/output.txt"
-
-# Create the output directory if it does not already exist
-if not os.path.exists("output"):
-    os.makedirs("output")
-
-# Write the text to a file
-with open(output_file, "w", encoding="utf-8") as outfile:
-    outfile.write(all_text)
+# Run the GUI
+root.mainloop()
